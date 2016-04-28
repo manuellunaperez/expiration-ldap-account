50de640005c332d62468bdebd73a594bb35dabfc
Manuel Luna Perez <manuel.luna@cica.es>
2016-04-28 14:48:07 +0200

    Actualizacion

--- expirationldapaccount.sh
+++ expirationldapaccount.sh
@@ -6,6 +6,9 @@ adminldap="cn=Manager,dc=cica,dc=es"
 passldap="-"
 ramaldap="ou=supercomputacion,ou=externos,ou=users,ou=cuentas,dc=cica,dc=es"
 
+fechaalta=`ldapsearch -H $servidorldap -x -D "$adminldap" -w "$passldap" -b "uid=$usuario,$ramaldap" "modifyTimestamp" |egrep ^modifyTimestamp |cut -d " " -f 2 |cut -b 1-8`
+fechaaltaUE=`date --date=fechaalta +%s`
 
-ldapsearch -H $servidorldap -x -D "$adminldap" -w "$passldap" -b "uid=$usuario,$ramaldap" "modifyTimestamp" |egrep ^modifyTimestamp |cut -d " " -f 2 |cut -b 1-8
-
+diaactual=`date +%s`
+diashaciaatras=7776000 #90 dias=7776000
+ventanatemporal=$(( diaactual - diashaciaatras ))
