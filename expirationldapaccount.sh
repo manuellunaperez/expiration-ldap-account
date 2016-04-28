e8a2c80c61a42c1bd47709d7b1eeada2b7e3579e
Manuel Luna Perez <manuel.luna@cica.es>
2016-04-28 14:30:09 +0200

    Consulta timestamp

--- expirationldapaccount.sh
+++ expirationldapaccount.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+usuario=$1
 servidorldap="ldaps://ldap1.hpc.cica.es:636"
 adminldap="cn=Manager,dc=cica,dc=es"
 passldap="-"
 ramaldap="ou=supercomputacion,ou=externos,ou=users,ou=cuentas,dc=cica,dc=es"
 
-ldapsearch -H $servidorldap -x -D "$adminldap" -w "$passldap" -b "uid=yenny,$ramaldap" "modifyTimestamp" |egrep ^modifyTimestamp
+
+ldapsearch -H $servidorldap -x -D "$adminldap" -w "$passldap" -b "uid=$usuario,$ramaldap" "modifyTimestamp" |egrep ^modifyTimestamp |cut -d " " -f 2 |cut -b 1-8
+
