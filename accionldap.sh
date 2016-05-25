ccf4cdc43f227cc669e2b1db901078d38783309f
Manuel Luna Perez <manuel.luna@cica.es>
2016-05-25 12:08:52 +0200

    Actualizacion de scripts

--- accionldap.sh
+++ accionldap.sh
@@ -7,58 +7,79 @@ ramaldap="ou=supercomputacion,ou=externos,ou=users,ou=cuentas,dc=cica,dc=es"
 
 declare -A Usuariosbloqueados
 
+informar() {
+        local nombre=$1
+        local pass=$2
+        local email=`ldapsearch -H $servidorldap -x -D "$adminldap" -w "$passldap" -b "$ramaldap" -s sub "uid=$nombre" mail |grep ^mail |cut -d " " -f 2`
+
+        echo -e "Estimado usuario: \n\nNos ponemos en contacto con usted para informale que su cuenta $nombre ha sido desbloqueada de los servicios de Supercomputación de CICA. \n\nEstos son los nuevos datos de acceso: \nUsuario: $nombre \nContraseña: $pass" | mail -a "Content-Type: text/plain; charset=UTF-8" -s "Cuenta en los servicios de Supercomputación de CICA" $email
+}
+
 desbloquearcuenta() {
 	local nombre=$1
-	local pass=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1` #Generamos una contraseña aleatoria 
-	local nuevapass=`slappasswd -h '{MD5}' -s "$pass"` #Ciframos la contraseña 
-	`ldapadd -H $servidorldap -x -D "$adminldap" -w "$passldap" << EOF                                                                                  
-dn: uid=$nombre,$ramaldap
-changeType: modify
-replace: userPassword
-userPassword: $nuevapass
-EOF`
+	local pass=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 6 | head -n 1` #Generamos una contraseña aleatoria 
+	`2>/dev/null 1>/dev/null ldappasswd  -H $servidorldap -x -D "$adminldap" -w $passldap -s "$pass" "uid=$nombre,$ramaldap"`
 
-	`ldapadd -H $servidorldap -x -D "$adminldap" -w "$passldap" << EOF                                                                                  
+	`2>/dev/null 1>/dev/null ldapadd -H $servidorldap -x -D "$adminldap" -w "$passldap" << EOF                                                                                  
 dn: uid=$nombre,$ramaldap
 changeType: modify
 replace: loginShell
 loginShell: /bin/bash
 EOF`
 
-	`ldapadd -H $servidorldap -x -D "$adminldap" -w "$passldap" << EOF                                                                                  
+	`2>/dev/null 1>/dev/null ldapadd -H $servidorldap -x -D "$adminldap" -w "$passldap" << EOF                                                                                  
 dn: uid=$nombre,$ramaldap
 changeType: modify
 delete: pwdAccountLockedTime
 EOF`
 
-	echo "La cuenta del usuario $nombre ha sido renovada."
-	echo "Contraseña: $pass"
+	echo "La cuenta del usuario $nombre ha sido desbloqueada."
+	echo "Usuario: $nombre - Contraseña: $pass" >> usuarios-pass
+	informar $nombre $pass
+
 }
 
 eliminarcuenta() {
 	local nombre=$1
 	`ldapdelete -H $servidorldap -x -D "$adminldap" -w "$passldap" "uid=$nombre,ou=supercomputacion,ou=externos,ou=users,ou=cuentas,dc=cica,dc=es"`
 
-	echo "La cuenta del usuario $nombre ha sido bloqueada."
+	echo "La cuenta del usuario $nombre ha sido eliminada."
 }	
 Acciones() {
 	for nombre in "${!Usuariosbloqueados[@]}"; do
 		echo "Acciones a realizar con la cuenta de $nombre"
-		echo $'Pulse 0 para salir. \nPulse 1 para eliminar la cuenta. \nPulse 2 para desbloquear la cuenta.'
-		read -n1 -p "Introduzca un opción ha realizar con el usuario: " ACCION
+		echo $'Pulse 0 para salir. \nPulse 1 para eliminar la cuenta. \nPulse 2 para desbloquear la cuenta.\n'
+		read -n1 -p "Introduzca un opción ha realizar con el usuario: \n" ACCION
 		case $ACCION in
 			0)
 				clear
 				echo $'Adios.\n'
 				exit;;
 			1)
-				clear
-				echo "La cuenta del usuario $nombre ha sido eliminada."
-				eliminarcuenta $nombre
+				read -n1 -p "¿Está seguro que desea borrar este usuario?: [s/n]\n" ACCION2
+				case $ACCION2 in
+					s)
+						clear
+						eliminarcuenta $nombre
+						;;
+					n)
+						clear
+						echo "Usted ha decidido no eliminar la cuenta $nombre"
+						;;
+				esac
 				;;
 			2)
-				clear
-				desbloquearcuenta $nombre
+				read -n1 -p "¿Está seguro que desea desbloquear este usuario?: [s/n]\n" ACCION3
+				case $ACCION3 in
+					s)
+						clear
+						desbloquearcuenta $nombre
+						;;
+					n)
+						clear
+						echo "Usted ha decido no desbloquear esta cuenta, $nombre seguirá bloqueada."
+						;;
+				esac
 				;;
 			*)
 				clear
--- mailldapaccount.sh
+++ mailldapaccount.sh
@@ -12,50 +12,74 @@ touch info_email.txt
 echo "Cuentas que expiran en los próximos 30 días:" > info_email.txt
 
 Calculardías() {
-        local nombre=$1
-        local fecha=$2
-        local fechacaducidad=`date +%Y/%m/%d -d "$fecha + 1 year"`
-        local fechacaducidadUE=`date +%s -d "$fecha + 1 year"`
-        local diferencia=$(( ( fechacaducidadUE - díaactual) / 86400 ))
-        local email=`ldapsearch -H $servidorldap -x -D "$adminldap" -w "$passldap" -b "$ramaldap" -s sub "uid=$nombre" mail |grep ^mail |cut -d " " -f 2`
-        WARNING $nombre $email $fechacaducidad
-        echo "La fecha de expiración de la cuenta del usuario $nombre se aproxima: $fechacaducidad" >> info_email.txt
-        echo "Quedan $diferencia días para que expire la cuenta, un mail fue enviado a $email automáticamente avisando a este usuario." >> info_email.txt
+	local nombre=$1
+	local fecha=$2
+	local fechacaducidad=`date +%Y/%m/%d -d "$fecha + 1 year"`
+	local fechacaducidadUE=`date +%s -d "$fecha + 1 year"`
+	local diferencia=$(( ( fechacaducidadUE - díaactual) / 86400 ))
+	local email=`ldapsearch -H $servidorldap -x -D "$adminldap" -w "$passldap" -b "$ramaldap" -s sub "uid=$nombre" mail |grep ^mail |cut -d " " -f 2`
+	WARNING $nombre $email $fechacaducidad
+	echo "La fecha de expiración de la cuenta del usuario $nombre se aproxima: $fechacaducidad" >> info_email.txt
+	echo "Quedan $diferencia días para que expire la cuenta, un mail fue enviado a $email automáticamente avisando a este usuario." >> info_email.txt
 }
 
+bloquearcuenta() {
+	for nombre in "${!Usuariosexpirados[@]}"; do
+		email=`ldapsearch -H $servidorldap -x -D "$adminldap" -w "$passldap" -b "$ramaldap" -s sub "uid=$nombre" mail |grep ^mail |cut -d " " -f 2`
+
+		`2>/dev/null 1>/dev/null ldapadd -H $servidorldap -x -D "$adminldap" -w "$passldap" << EOF                                                                                  
+dn: uid=$nombre,$ramaldap
+changeType: modify
+add: pwdAccountLockedTime
+pwdAccountLockedTime: 000001010000Z
+EOF`
+		`2>/dev/null 1>/dev/null ldapadd -H $servidorldap -x -D "$adminldap" -w "$passldap" << EOF                                                                                  
+dn: uid=$nombre,$ramaldap
+changeType: modify
+replace: loginShell
+loginShell: /bin/false
+EOF`
+		echo -e "\nEl usuario $nombre ha expirado, puede ponerse en contacto con este usuario a través de $email." >> info_email.txt
+		MAIL_EXPIRADO $nombre $email
+	done
+}	
+
 WARNING() {
-        local nombre=$1
-        local email=$2
-        local fecha=$3
-        echo -e "Estimado usuario: \n\nNos ponemos en contacto con usted para informarle que su cuenta llamada $nombre expira el día $fecha \n Para poder utilizando dicho usuario en los servicios de Supercomputación debe renovar su cuenta.\nPara renovar su cuenta debe ponerse en contacto con los servicios de supercomputación a través de la dirección de correo eciencia@cica.es \n" | mail -a "Content-Type: text/plain; charset=UTF-8" -s "Fecha de expiración de su cuenta en servicios de Supercomputación de CICA" $email
+	local nombre=$1
+	local email=$2
+	local fecha=$3
+	echo -e "Estimado usuario: \n\nNos ponemos en contacto con usted para informarle que su cuenta llamada $nombre expira el día $fecha \n Para poder utilizando dicho usuario en los servicios de Supercomputación debe renovar su cuenta.\nPara renovar su cuenta debe ponerse en contacto con los servicios de supercomputación a través de la dirección de correo eciencia@cica.es \n" | mail -a "Content-Type: text/plain; charset=UTF-8" -s "Fecha de expiración de su cuenta en servicios de Supercomputación de CICA" $email
 
 }
 WARNING_CICA() {
-        local email="eciencia@cica.es"
-        cat info_email.txt| mail -a "Content-Type: text/plain; charset=UTF-8" -s "Cuentas en servicios de Supercomputación de CICA" $email
+	local email="eciencia@cica.es"
+	cat info_email.txt| mail -a "Content-Type: text/plain; charset=UTF-8" -s "Cuentas en servicios de Supercomputación de CICA" $email
 }
 
 
-EXPIRADO() {
-        local nombre=$1
-        local email=$2
-        echo -e "Estimado usuario: \n\nNos ponemos en contacto con usted para informale que su cuenta $nombre ha expirado en los servicios de Supercomputación de CICA. \nPara renovar su cuenta debe ponerse en contacto con los servicios de supercomputacion a través de la direcion de correo eciencia@cica.es.\n" | mail -a "Content-Type: text/plain; charset=UTF-8" -s "Expiración de cuenta en servicios de Supercomputacion de CICA" $email
+MAIL_EXPIRADO() {
+	local nombre=$1
+	local email=$2
+	echo -e "Estimado usuario: \n\nNos ponemos en contacto con usted para informale que su cuenta $nombre ha expirado en los servicios de Supercomputación de CICA. \nPara renovar su cuenta debe ponerse en contacto con los servicios de supercomputacion a través de la direcion de correo eciencia@cica.es.\n" | mail -a "Content-Type: text/plain; charset=UTF-8" -s "Expiración de cuenta en servicios de Supercomputacion de CICA" $email
 
 }
 
 obtenerdatos=`ldapsearch -H $servidorldap  -x -D "$adminldap" -w "$passldap" -b "$ramaldap" "modifyTimestamp" |egrep "^dn:|^modifyTimestamp:" | tr -d " " | cut -d "=" -f 2 | tr -d "\n" | sed s/Z/"\n"/g`
 for i in $obtenerdatos; do
-        nombre=`echo $i | cut -d "," -f 1`
-        fecha=`echo $i | cut -d ":" -f 2 | cut -c 1-8`
-        fechaaltaUE=`date --date="$fecha" +%s`
-        if [[ $nombre != "supercomputacion" ]]; then
-                        if [[ $fechaaltaUE -le $margenmin ]] && [[ $fechaaltaUE -gt $margenmax ]]; then
-                                Calculardías $nombre $fecha
-                        fi
-                        if [[ $fechaaltaUE -lt $margenmax ]]; then
-                                Usuariosexpirados[$nombre]=1
-                        fi
-        fi
+	nombre=`echo $i | cut -d "," -f 1`
+	fecha=`echo $i | cut -d ":" -f 2 | cut -c 1-8`
+	fechaaltaUE=`date --date="$fecha" +%s`
+	if [[ $nombre != "supercomputacion" ]]; then
+		if [[ $fechaaltaUE -le $margenmin ]] && [[ $fechaaltaUE -gt $margenmax ]]; then
+			Calculardías $nombre $fecha
+		fi
+		if [[ $fechaaltaUE -lt $margenmax ]]; then
+			comprobarshell=`ldapsearch -H $servidorldap -x -D "$adminldap" -w "$passldap" -b "$ramaldap" "uid=$nombre" loginShell | egrep ^loginShell | cut -d " " -f 2 `
+			if [[ $comprobarshell == "/bin/bash" ]]; then
+				Usuariosexpirados[$nombre]=1
+			fi
+		fi
+	fi
 done
 
 echo -e  "\n---------------------Usuarios Expirados---------------------------\n" >> info_email.txt
@@ -63,13 +87,10 @@ echo -e  "\n---------------------Usuarios Expirados---------------------------\n
 Comprobarultimaconexion=`ssh sesamo "bash /opt/scripts/usuarios-ultimaConexion.sh" | egrep -v '(Nunca ha entrado|root|Nombre)' | tr -s ' ' | cut -d " " -f 1 | tail -n+4`
 for usuario in $Comprobarultimaconexion; do #Comprobamos los usuarios que llevan más de 1 año sin entrar a sesamo y que no estén bloqueados.
 	comprobarshell=`ldapsearch -H $servidorldap -x -D "$adminldap" -w "$passldap" -b "$ramaldap" "uid=$usuario" loginShell | egrep ^loginShell | cut -d " " -f 2 `
-	email=`ldapsearch -H $servidorldap -x -D "$adminldap" -w "$passldap" -b "$ramaldap" -s sub "uid=$usuario" mail |grep ^mail |cut -d " " -f 2`
 	if [[ $comprobarshell == "/bin/bash" ]]; then
 		Usuariosexpirados[$usuario]=1
-		echo -e "\nEl usuario $usuario ha expirado, puede ponerse en contacto con este usuario a través de $email." >> info_email.txt
-		EXPIRADO $usuario $email
 	fi
 done
-
+bloquearcuenta
 WARNING_CICA
 rm info_email.txt
