40895c1bc5465f9a2e252bcd685ec34316d57124
Manuel Luna Perez <manuel.luna@cica.es>
2016-06-13 14:38:29 +0200

    Script para eliminar el estado de los usuarios ldap

---
+++ eliminarestado.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+servidorldap="ldaps://ldap1.hpc.cica.es:636"
+adminldap="cn=Manager,dc=cica,dc=es"
+passldap="-"
+ramaldap="ou=supercomputacion,ou=externos,ou=users,ou=cuentas,dc=cica,dc=es"
+
+obtenerestado=`ldapsearch -H $servidorldap  -x -D "$adminldap" -w "$passldap" -b "$ramaldap" "uid" | egrep ^uid | tr -d " " | cut -d ":" -f 2`
+for i in $obtenerestado; do
+	if [[ $i != "supercomputacion" ]]; then
+	`2>/dev/null 1>/dev/null ldapadd -H $servidorldap -x -D "$adminldap" -w "$passldap" << EOF                                                                                  
+dn: uid=$i,$ramaldap
+changeType: modify
+delete: accountStatus
+EOF`
+	fi
+done
